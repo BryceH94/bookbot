e1c3ae3fb18c82cace9b9b985ea56b5a2d089267
BryceH94 <BryceHargis@outlook.com>
2024-04-16 21:24:19 -0700

    Count total words

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
-    with open("books/frankenstein.txt", "r") as f:
-        book = f.read()
-    print(book)
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_contents(book_path) 
+    word_count = get_word_count(book_contents)
+    print(f"{word_count} words found in book")
+    return word_count
+
+def get_book_contents(path):
+    with open(path, "r") as f:
+        contents = f.read()
+    return contents
+
+def get_word_count(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
 
 main()  
