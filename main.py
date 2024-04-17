45057e9106e4232f50daf9967ef6c4ed06d02066
BryceH94 <BryceHargis@outlook.com>
2024-04-16 21:38:38 -0700

    Count letters

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     book_contents = get_book_contents(book_path) 
     word_count = get_word_count(book_contents)
     print(f"{word_count} words found in book")
-    return word_count
+    letter_counts = get_letter_counts(book_contents)
+    print("Printing massive letter count dict...")
+    print(letter_counts)
 
 def get_book_contents(path):
     with open(path, "r") as f:
@@ -14,5 +16,15 @@ def get_word_count(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def get_letter_counts(text):
+    letter_counts = {}
+    for char in text:
+        char = char.lower()
+        if char in letter_counts:
+            letter_counts[char] += 1
+        else:
+            letter_counts[char] = 1
+    return letter_counts
 
 main()  
